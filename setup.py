8ac1fb97827db5e480993e0730d47512a89be862
Qiusheng Wu <giswqs@gmail.com>
2020-11-14 18:25:52 -0500

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/eefolium",
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
