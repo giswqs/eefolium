52428d4c6438aea05a41dcc7d0db88b60462d239
Qiusheng Wu <giswqs@gmail.com>
2020-11-14 18:09:03 -0500

    Bump version: 0.0.2 → 0.0.3

--- eefolium/__init__.py
+++ eefolium/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
-__version__ = "__version__ = '0.0.2'"
+__version__ = "__version__ = '0.0.3'"
 
 from .eefolium import *
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/eefolium",
-    version="0.0.2",
+    version="version='0.0.3'",
     zip_safe=False,
 )
