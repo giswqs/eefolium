61642e569a7536c12032911c7128c671ac0521f9
Qiusheng Wu <giswqs@gmail.com>
2020-11-14 18:06:19 -0500

    Fix version issue

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/eefolium",
-    version="version='0.0.2'",
+    version="0.0.2",
     zip_safe=False,
 )
